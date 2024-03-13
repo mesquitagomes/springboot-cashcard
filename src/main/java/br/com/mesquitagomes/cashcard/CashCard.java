bcb2f08b6dc5b282f4170a190e5cc0362c780f0a
Marcel <mesquitagomes@gmail.com>
2024-03-13 09:25:12 -0300

    Cash card Json test

---
+++ src/main/java/br/com/mesquitagomes/cashcard/CashCard.java
@@ -0,0 +1,29 @@
+package br.com.mesquitagomes.cashcard;
+
+public class CashCard {
+
+    private Long id;
+    private Double amount;
+
+    public CashCard(Long id, Double amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+}
